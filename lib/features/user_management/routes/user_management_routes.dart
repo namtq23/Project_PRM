c6fa4a4e388700d4b9e2979ca7621be5b408bd81
DESKTOP-3JSH5PD\admin <vanduc06102005@gmail.com>
2026-07-22 00:05:03 +0700

    refactor(user_management): standardize folder structure and add feature routes to match bookings module

---
+++ lib/features/user_management/routes/user_management_routes.dart
@@ -0,0 +1,13 @@
+import 'package:go_router/go_router.dart';
+
+import '../../../app/router/route_names.dart';
+import '../../../app/router/route_paths.dart';
+import '../presentation/views/user_management_screen.dart';
+
+List<RouteBase> userManagementRoutes() => [
+  GoRoute(
+    path: RoutePaths.adminUsers,
+    name: RouteNames.adminUsers,
+    builder: (context, state) => const UserManagementScreen(),
+  ),
+];
